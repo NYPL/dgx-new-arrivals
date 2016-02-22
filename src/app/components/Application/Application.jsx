bf4f1d4354cdec6d331186958174a665d4c130a8
Edwin Guzman <edwin.gzmn@gmail.com>
2016-02-22 17:46:39 -0500

    Cleaning up code and rendering list of book covers.

--- src/app/components/Application/Application.jsx
+++ src/app/components/Application/Application.jsx
@@ -19,17 +19,7 @@ class App extends React.Component {
 
     this.state = {
       all: _.flatten(store.newArrivalsData),
-      filtered: _.flatten(store.newArrivalsData),
-      value: [],
-      genres: [],
     };
-
-    this._generateItemsToDisplay = this._generateItemsToDisplay.bind(this);
-    this._selectChange = this._selectChange.bind(this);
-    this._selectGenreChange = this._selectGenreChange.bind(this);
-    this._getAttrList = this._getAttrList.bind(this);
-    this._createLabel = this._createLabel.bind(this);
-    this._createSelectObj = this._createSelectObj.bind(this);
   }
 
   // Event listeners
@@ -42,104 +32,38 @@ class App extends React.Component {
   }
 
   _onChange() {
-    this.setState({
-      filtered: this.state.filtered
-    });
+    this.setState({});
   }
 
-  _selectChange(value) {
-    console.log(`Selected: ${value}`);
-    let data = this.state.all;
-
-    if (value.length) {
-      data = _.where(this.state.all, {contentType: value.toUpperCase()})
-    } 
-
-    this.setState({
-      filtered: data,
-      value
-    });
-  }
-
-  _selectGenreChange(genres) {
-    console.log(`Selected genre: ${genres}`);
-    let data = this.state.all;
-
-    if (genres.length) {
-      data = _.where(this.state.all, {genre: genres})
-    } 
-
-    this.setState({
-      filtered: data,
-      genres
-    });
-  }
-
-  _generateItemsToDisplay(bookLists) {
-    const bookCoverItems = (bookLists && bookLists.length) ?
-        bookLists.map((element, i) => {
-          const target = element.link || '#';
-
-          return (
-            <div key={i} className="slick-book-item">
-              <a href={target} className="bookItem" target="_parent">
-                <BookCover
-                  id={`item-${i}`}
-                  imgSrc={element.imageURL}
-                  alt=""
-                  className="cover-image" />
-              </a>
-            </div>
-          );
-        })
-        : null;
-
-    if (bookLists && bookLists.length) {
-      styles.bookItemsWidth.width = `${bookCoverItems.length * 149 - 29}px`;
-    }
-
-    return bookCoverItems;
-  }
-
-  _createLabel(str) {
-    const key = str.replace(/[_]/g, '-');
-    return key;
-  }
-
-  _createSelectObj(list) {
-    const arr = _.map(list, l => {
-        const obj = {};
-        obj.value = l;
-        obj.label = this._createLabel(l);
-
-        return obj;
-      });
-
-    return arr;
-  }
-
-  _getAttrList(list, attr) {
-    const values = _.chain(list)
-      .pluck(attr)
-      .unique()
+  _generateItemsToDisplay() {
+    const bookCoverItems = _.chain(NewArrivalsStore.getState().newArrivalsData)
+      .flatten()
       .value();
 
-    return this._createSelectObj(values);
+    const books = bookCoverItems.map((element, i) => {
+        const target = '#';
+        return (
+          <li className='book-item' key={i}>
+            <a href={target} className="bookItem">
+              <BookCover
+                imgSrc={element.imageURL}
+                alt=""
+                className="cover-image" />
+            </a>
+          </li>
+        );
+      });
+
+    return books;
   }
+
   
   render() {
-    const newArrivals = this.state.filtered;
-
-    // Used when the opts, genre, format, etc, are being
-    // computed
-    let isLoading = true;
-    if (newArrivals.length) {
-      isLoading = false;
-    }
+    const books = this._generateItemsToDisplay();
 
     return (
       <div className='app-wrapper'>
-        Test
+        {books}
       </div>
     );
   }
