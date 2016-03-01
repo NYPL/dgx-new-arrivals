cf27241e3b8cc64216a7838b6676f1f69c00f92e
Edwin Guzman <edwin.gzmn@gmail.com>
2016-03-01 15:29:51 -0500

    REverting back.

--- webpack.config.js
+++ webpack.config.js
@@ -100,7 +100,7 @@ if (ENV === 'production') {
         {
           test: /\.jsx?$/,
           exclude: /(node_modules|bower_components)/,
-          loader: 'babel',
+          loader: 'babel-loader',
           query: {
             presets: ['es2015', 'react']
           }
