3ecd95594cc8947be4c1a8ce0cafc8d2b3c006d9
Edwin Guzman <edwin.gzmn@gmail.com>
2016-03-16 18:43:20 -0400

    Temporarily adding svg close button and styling for filter layout.

--- src/app/components/ToggleDisplay/ToggleDisplay.jsx
+++ src/app/components/ToggleDisplay/ToggleDisplay.jsx
@@ -8,14 +8,86 @@ import PillButton from '../Buttons/PillButton.jsx';
 import NewArrivalsStore from '../../stores/Store.js';
 import Actions from '../../actions/Actions.js';
 
-class Filter extends React.Component {
+class IconButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this._onClick = this._onClick.bind(this);
+  }
+
+  _onClick(e) {
+    e.preventDefault();
+    this.props.onClick();
+  }
+
+  render() {
+    return (
+      <button
+        className={`${this.props.className} svgIcon`}
+        onClick={this._onClick}
+      >
+        {this.props.icon}
+      </button>
+    );
+  }
+}
+
+
+class CloseButton extends React.Component {
   constructor(props) {
     super(props);
   }
 
   render() {
+    const icon = (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 50 50"
+        height={10}
+        width={10}
+      >
+        <polygon points="50 5.84 44.16 0 25 19.16 5.84 0 0 5.84 19.16 25 0 44.16 5.84 50 25 30.84 44.16 50 50 44.16 30.84 25 50 5.84"/>
+      </svg>
+    );
+
+    return (
+      <IconButton
+        {...this.props}
+        icon={icon}
+      />
+    );
+  }
+}
+
+
+class Filter extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this._closeFilters = this._closeFilters.bind(this);
+  }
+
+  _closeFilters() {
+    Actions.toggleFilters(false);
+  }
+
+  render() {
     return (
       <div className={`filter-wrapper ${this.props.active}`}>
+        <div className="filter-header">
+          <h2>Filter by</h2>
+          <CloseButton onClick={this._closeFilters}/>
+        </div>
+
+        <ul>
+          <li>BOOK/TEXT</li>
+          <li>E-BOOK</li>
+          <li>LARGE PRINT</li>
+          <li>AUDIOBOOK</li>
+          <li>E-AUDIOBOOK</li>
+          <li>DVD</li>
+          <li>BLU-RAY</li>
+          <li>MUSIC CD</li>
+        </ul>
 
       </div>
     );
