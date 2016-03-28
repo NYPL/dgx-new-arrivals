a13fdbe65ea9947bf3eb8ef928d643ad3bb052b2
Edwin Guzman <edwin.gzmn@gmail.com>
2016-03-28 16:45:12 -0400

    Adding better icons to buttons.

--- src/app/components/Filter/Filter.jsx
+++ src/app/components/Filter/Filter.jsx
@@ -190,11 +190,21 @@ class Filter extends React.Component {
 
         <div className="submit-buttons">
           <button className="PillButton apply" onClick={this._submitFilters}>
-            Apply
+            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" class="svgIcon">
+              <title>apply.icon.svg</title>
+              <path d="M23.26,13.1819a1.2736,1.2736,0,0,0-1.7332,0L17,17.6253V6.1041a1.0119,1.0119,0,1,0-2,0V17.6253l-4.5268-4.4434a1.2212,1.2212,0,0,0-1.6916,0,1.17,1.17,0,0,0-.0208,1.65L15.1786,21.26l0,0.0083a1.1694,1.1694,0,0,0,1.6488,0l0.0048-.0083L23.26,14.8318A1.17,1.17,0,0,0,23.26,13.1819Z" />
+              <rect x="14.8333" y="16.3602" width="2.3333" height="16.6711" rx="1.1667" ry="1.1667" transform="translate(-8.6957 40.6957) rotate(-90)" />
+            </svg>
+            <span>Apply</span>
           </button>
 
           <button className="PillButton reset" onClick={this._resetFilters}>
-            Reset All
+            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" class="svgIcon">
+              <title>apply.icon.svg</title>
+              <path d="M23.26,13.1819a1.2736,1.2736,0,0,0-1.7332,0L17,17.6253V6.1041a1.0119,1.0119,0,1,0-2,0V17.6253l-4.5268-4.4434a1.2212,1.2212,0,0,0-1.6916,0,1.17,1.17,0,0,0-.0208,1.65L15.1786,21.26l0,0.0083a1.1694,1.1694,0,0,0,1.6488,0l0.0048-.0083L23.26,14.8318A1.17,1.17,0,0,0,23.26,13.1819Z" />
+              <rect x="14.8333" y="16.3602" width="2.3333" height="16.6711" rx="1.1667" ry="1.1667" transform="translate(-8.6957 40.6957) rotate(-90)" />
+            </svg>
+            <span>Reset All</span>
           </button>
         </div>
 
