2b1f57e277f522fc8b837fd101736d13a41fd5fe
Edwin Guzman <edwin.gzmn@gmail.com>
2016-03-28 15:33:15 -0400

    Adding separate component for displayed filters which are selected.

--- src/app/components/NewArrivals/NewArrivals.jsx
+++ src/app/components/NewArrivals/NewArrivals.jsx
@@ -2,51 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import _ from 'underscore';
-import axios from 'axios';
 
 import NewArrivalsStore from '../../stores/Store.js';
 import Actions from '../../actions/Actions.js';
 import Isotopes from '../Isotopes/Isotopes.jsx';
 import ToggleDisplay from '../ToggleDisplay/ToggleDisplay.jsx';
-
-
-class SelectedFilters extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this._onChange = this._onChange.bind(this);
-    this._getFilterList = this._getFilterList.bind(this);
-    this.state = NewArrivalsStore.getState();
-  }
-
-  componentDidMount() {
-    NewArrivalsStore.listen(this._onChange);
-  }
-
-  componentWillUnmount() {
-    NewArrivalsStore.unlisten(this._onChange);
-  }
-
-  _onChange() {
-    this.setState(NewArrivalsStore.getState());
-  }
-
-  _getFilterList(filters) {
-    return _.map(this.state.filters, (filter, i) => {
-      if (filter) {
-        return <li key={i}>{filter}</li>;
-      }
-
-      return null;
-    });
-  }
-
-  render() {
-    const filters = this._getFilterList(this.state.filters);
-
-    return filters.length ? <ul className="selectedFilters">{filters}</ul> : null;
-  }
-}
+import SelectedFilters from '../SelectedFilters/SelectedFilters.jsx';
 
 /**
  * Renders the main section of the New Arrivals app.
@@ -62,7 +23,6 @@ class NewArrivals extends React.Component {
     };
 
     this._onChange = this._onChange.bind(this);
-    this._updateBooks = this._updateBooks.bind(this);
   }
 
   componentDidMount() {
@@ -78,14 +38,6 @@ class NewArrivals extends React.Component {
       displayType: NewArrivalsStore.getState().displayType,
       all: NewArrivalsStore.getState().newArrivalsData.bibItems
     });
-  }
-
-  _updateBooks(format, itemCount) {
-    axios
-      .get(`/api?format=${format}&itemCount=${itemCount}`)
-      .then(response => {
-        Actions.updateNewArrivalsData(response.data);
-      }); /* end axios call */
   }
 
   render() {
