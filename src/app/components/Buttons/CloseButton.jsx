f3118069b11873ef93d96aa3fc40c758cdf60b24
Edwin Guzman <edwin.gzmn@gmail.com>
2016-05-20 17:06:40 -0400

    Updating SVG icon for the X close button.

--- src/app/components/Buttons/CloseButton.jsx
+++ src/app/components/Buttons/CloseButton.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import { SoloXIcon } from 'dgx-svg-icons';
+import { XIcon } from 'dgx-svg-icons';
 
 import IconButton from './IconButton.jsx';
 
 const CloseButton = ({ className, onClick }) => (
-  <IconButton className={className} icon={<SoloXIcon />} onClick={onClick} />
+  <IconButton
+    className={className}
+    icon={<XIcon width='48' height='48' />}
+    onClick={onClick}
+  />
 );
 
 CloseButton.propTypes = {
