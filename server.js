6b49157381477cd38f0e73f6c5923910d65f9e3c
Edwin Guzman <edwin.gzmn@gmail.com>
2016-07-07 10:59:35 -0400

    Updating the dist folder path and where the app reads the files from.

--- server.js
+++ server.js
@@ -38,8 +38,8 @@ app.set('views', VIEWS_PATH);
 
 app.set('port', process.env.PORT || 3001);
 
-app.use('/browse/new-arrivals/', express.static(DIST_PATH));
 app.use(express.static(DIST_PATH));
+app.use('/browse/new-arrivals/', express.static(DIST_PATH));
 
 // For images
 app.use('*/src/client', express.static(INDEX_PATH));
