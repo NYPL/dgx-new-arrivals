deacff4411a3486ed53337e87d6ca78b8486a795
Edwin Guzman <edwin.gzmn@gmail.com>
2016-08-29 11:50:54 -0400

    Removing the pagination button if the returned items count is less than 18. Assuming that there won't be more to make another call.

--- src/app/components/NewArrivals/NewArrivals.jsx
+++ src/app/components/NewArrivals/NewArrivals.jsx
@@ -108,7 +108,8 @@ class NewArrivals extends React.Component {
     }, error => Promise.reject(error));
 
     makeApiCall(queries, response => {
-      const displayPagination = response.data.bibItems.length !== 0;
+      const bibItems = response.data.bibItems;
+      const displayPagination = !(bibItems.length < 18);
       Actions.updateDisplayPagination(displayPagination);
       Actions.addMoreItems(response.data.bibItems);
       Actions.updatePageNum(true);
